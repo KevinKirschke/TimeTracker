9c5ce6ee6e8b394d5a683d740f6c532095e49237
KevinKirschke <kevin.kirschke@mail.de>
2025-01-16 16:24:28 +0100

    ttSTART

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Time Tracker App</h1>
+      <h2>is comming soon here !</h2>
       </header>
+
+      <main>
+      <h1>Time Tracker App</h1>
+      <h2>is comming soon here !</h2>
+      </main>
+
+      <footer>
+      <h1>Time Tracker App</h1>
+      <h2>is comming soon here !</h2>
+      </footer>
+
     </div>
   );
 }
